865411c00ea97cc7d64104f5039d5b2c808745d7
Manoj Kumar Pabbineedi <manojpabbineedi41@gmail.com>
2026-06-24 10:26:59 +0530

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.py
@@ -0,0 +1,24 @@
+from itertools import permutations
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        x = list(permutations(nums))
+        return list(map(list, x))
+        # n=len(nums)
+        # arr=[]
+        # res=[]
+        # visited=set()
+        # def f():
+        #     if len(nums)==len(arr):
+        #         res.append(arr[:])
+        #         return
+        #     for num in nums:
+        #         if num not in visited:
+        #             arr.append(num)
+        #             visited.add(num)
+        #             f()
+        #             arr.pop()
+        #             visited.remove(num)
+        # f()
+        # return res
+        
