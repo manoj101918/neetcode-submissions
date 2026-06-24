17af2f1f1883c29f8cfdbd268fe4df7b5a4aa43c
Manoj Kumar Pabbineedi <manojpabbineedi41@gmail.com>
2026-06-24 09:31:14 +0530

    Add: combinations - submission-0

---
+++ Data Structures & Algorithms/combinations/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        arr=[i for i in range(1,n+1)]
+        res=[]
+        nums=[]
+        ln=len(arr)
+        def f(idx):
+            if len(nums)==k:
+                res.append(nums[:])
+                return
+            if idx==ln:
+                return
+            nums.append(arr[idx])
+            f(idx+1)
+            nums.pop()
+            f(idx+1)
+
+        f(0)
+        return res
+
+        
